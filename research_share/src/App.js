ceafbcdccc2812cadfc586d35615c858c6fb0db4
Alexandre CHETRIT <alexandre.chetrit@epitech.eu>
2020-10-31 19:27:35 +0100

    feat: upload button + prototype download button

--- research_share/src/App.js
+++ research_share/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 
 const ipfs = ipfsClient(
   {
-    host : 'ipfs.infura.io/ipfs/',
-    port : '5001',
+    host : 'ipfs.infura.io',
+    port : 5001,
     protocol : 'https'
   }
 );
@@ -14,6 +14,7 @@ const ipfs = ipfsClient(
 function UploadFileButton({extractor}) {
   const [data, setData] = React.useState(null);
   React.useEffect(() => extractor(data), [data, extractor]);
+
   const handleClick = (event) => {
     event.preventDefault();
     let reader = new window.FileReader();
@@ -37,13 +38,16 @@ function UploadFileButton({extractor}) {
   )
 }
 
-function UploadToIpfsButton(props) {
+function UploadToIpfsButton({extractor}, {data}) {
+  const [hash, setHash] = React.useState(null);
+  React.useEffect(() => extractor(hash), [hash, extractor]);
+
   const UploadToIpfs = async (data) => {
       const results = await ipfs.add(data);
-      //console.log('you need to upload the node here', {data});
+      setHash(results.path);
   }
   const handleClick = (event) => {
-    UploadToIpfs(props.data);
+    UploadToIpfs(data);
   }
 
   return (
@@ -55,15 +59,37 @@ function UploadToIpfsButton(props) {
   )
 }
 
+function DownloadButton({hash}) {
+  const DownloadFromIpfs = async (hash) => {
+    // do something to get the object with the hash
+    //const result = ipfs.get({path : {hash}});
+    //console.log(result);
+  }
+  const handleClick = (event) => {
+    DownloadFromIpfs(hash);
+  }
+
+  return (
+    <div className="UploadToIpfsButton">
+      <Button component="label" onClick={handleClick}>
+        {'Download From Ipfs'}
+      </Button>
+    </div>
+  )
+}
+
 function App() {
   const [stringFile, setStringFile] = React.useState("");
+  const [nodeHash, setNodeHash] = React.useState("");
 
   return (
     <div className="App">
       <UploadFileButton extractor={setStringFile}>
       </UploadFileButton>
-      <UploadToIpfsButton data={stringFile}>
+      <UploadToIpfsButton data={stringFile} extractor={setNodeHash}>
       </UploadToIpfsButton>
+      <DownloadButton hash={nodeHash}>
+      </DownloadButton>
     </div>
   );
 }
