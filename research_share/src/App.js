58e1fb6ebe3859384b3f4abac1450e7245bdf8c0
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-11-16 17:55:10 +0100

    refactor: cleaned up App.js to use the 2 refactored buttons

--- research_share/src/App.js
+++ research_share/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import ipfsClient from 'ipfs-http-client';
 import './App.css';
-
-const all = require('it-all')
-const concat = require('it-concat')
+import {UploadButton} from './ButtonUpload';
+import { DownloadButton } from './ButtonDownload';
 
 const ipfs = ipfsClient(
   {
@@ -14,79 +12,15 @@ const ipfs = ipfsClient(
   }
 );
 
-function UploadFileButton({extractor}) {
-  const [data, setData] = React.useState(null);
-  React.useEffect(() => extractor(data), [data, extractor]);
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    let reader = new window.FileReader();
-    const files = event.target.files;
-
-    reader.readAsArrayBuffer(files[0]);
-    reader.onload = (ev) => {
-      let buffer = new Buffer.from(reader.result);
-      setData(() => buffer.toString());
-    };
-    }
-
-  return (
-    <div className="UploadButton">
-      <Button component="label">
-        <input type="file" name="Upload" style={{ display: "none" }} onChange={handleClick}>
-        </input>
-        {'Upload'}
-      </Button>
-    </div>
-  )
-}
-
-function UploadToIpfsButton({extractor, data}) {
-  const [hash, setHash] = React.useState(null);
-  React.useEffect(() => extractor(hash), [hash, extractor]);
-
-  const UploadToIpfs = async (data) => {
-      console.log(`data : ${data}`);
-      const results = await ipfs.add(data);
-      setHash(results.path);
-      console.log(`Path : ${results.path}`);
-  }
-  return (
-    <div className="UploadToIpfsButton">
-      <Button component="label" onClick={() => {UploadToIpfs(data);}}>
-        {'Upload To Ipfs'}
-      </Button>
-    </div>
-  )
-}
-
-function DownloadButton({hash}) {
-  const DownloadFromIpfs = async (hash) => {
-    const result = await all(ipfs.get(hash));
-    const file_content = await concat(result[0].content);
-    console.log(`file content : ${file_content}`);
-  }
-
-  return (
-    <div className="UploadToIpfsButton">
-      <Button component="label" onClick={() => {DownloadFromIpfs(hash);}}>
-        {'Download From Ipfs'}
-      </Button>
-    </div>
-  )
-}
-
 function App() {
-  const [stringFile, setStringFile] = React.useState("");
-  const [nodeHash, setNodeHash] = React.useState("");
-
-  return (
-    <div className="App">
-      <UploadFileButton extractor={setStringFile}/>
-      <UploadToIpfsButton data={stringFile} extractor={setNodeHash}/>
-      <DownloadButton hash={nodeHash}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Research Share</h1>
+            <UploadButton ipfs={ipfs}/>
+            <br/>
+            <DownloadButton ipfs={ipfs}/>
+        </div>
+    );
 }
 
 export default App;
