808199a0cd486f1e991c60921416a9c8bd3d495b
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-12-18 23:04:08 +0100

    fix: (tmp) import path for the contract's abi

--- src/components/user/UserContract.js
+++ src/components/user/UserContract.js
@@ -5,7 +5,7 @@ import PulseLoader from 'react-spinners/PulseLoader';
 
 import { UserInteract } from './UserInteract';
 
-const users_contract = require('../../contracts/users/users.json').contracts["Users.sol"].Users;
+const users_contract = require('../../contracts/god.json').contracts["users.sol"].Users;
 
 async function loadContract(address, setContract, setSpinner) {
     setSpinner(true);
