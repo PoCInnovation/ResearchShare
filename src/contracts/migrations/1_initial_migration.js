b35fd73ff6f23d77bd489f8741648f49f5ef9602
Matthis Cusin <matthis.cusin@epitech.eu>
2021-01-08 18:48:33 +0100

    [Update] Add Migrations contract and configuration

--- src/contracts/migrations/1_initial_migration.js
+++ src/contracts/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const ResearchShare = artifacts.require("ResearchShare");
+const Migrations = artifacts.require("Migrations");
 
 module.exports = function (deployer) {
+  deployer.deploy(Migrations);
   deployer.deploy(ResearchShare);
 };
