296f6a048d1dfd67c62326505676af9d867150e1
Alexandre CHETRIT <alexandre.chetrit@epitech.eu>
2021-02-13 12:45:49 +0100

    feat: basic file downloading with .txt files

--- src/components/download_paper/ButtonDownload.js
+++ src/components/download_paper/ButtonDownload.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import './ButtonDownload.css';
 
+const fileDownload = require('js-file-download');
 const all = require('it-all')
 const concat = require('it-concat')
 
@@ -12,15 +13,16 @@ const concat = require('it-concat')
  * @param setContent - Setter from `React.useState` to retrieve the content of the file.
  * @returns {Promise<void>}
  */
-async function downloadFromIPFS(ipfs, hash, setContent) {
+async function downloadFromIPFS(ipfs, hash) {
     const result = await all(ipfs.get(hash));
-    const file_content = await concat(result[0].content);
-    setContent(file_content.toString());
+    const fileContent = await concat(result[0].content);
+    //console.log(fileContent.toString());
+    return (fileContent);
 }
 
 /**
- *  Component Function used to retrieve a file from IPFS based on user's input.
- *  Test hash: QmPKby2sr2fxeEpxeGVuevGsvSVd1Hs37JBR9QSuWrvzuV -- my_world file from Alex & Theo
+ * Component Function used to retrieve a file from IPFS based on user's input.
+ * Test hash: QmPKby2sr2fxeEpxeGVuevGsvSVd1Hs37JBR9QSuWrvzuV -- my_world file from Alex & Theo
  * @param ipfs - IPFS Client.
  * @returns {JSX.Element}
  * @constructor
@@ -29,30 +31,24 @@ export function DownloadButton({ipfs}) {
     // TODO: Provide way to download by filename
     //       Back: Call smart contract to get hash by filename
     //       Font: Provide way to input either hash or filename
-    //             || Find way to differentiate filename & hash base on input
     const [hash, setHash] = React.useState("");
-    const [content, setContent] = React.useState("");
 
+    const handleClick = async (e) => {
+        if (!hash)
+            return;
+        const data = await downloadFromIPFS(ipfs, hash);
+        fileDownload(data, 'test.txt');
+    }
     const inputOnChange = (event) => setHash(event.target.value);
-    const buttonOnClick = async () => {
-        // TODO: add error handling if hash empty
-        await downloadFromIPFS(ipfs, hash, setContent);
-    };
 
     return (
         <div>
             <div id="download">
                 <input id="download_input" onChange={inputOnChange} type="text" name="HashField"/>
-                <Button id="download_button" onClick={buttonOnClick} variant="contained" color="primary">
+                <Button id="download_button" onClick={handleClick} variant="contained" color="primary">
                     Download
                 </Button>
             </div>
-            { content ?
-                <div className="FileContent">
-                    <br/><br/><h2>File Content: </h2><br/>
-                    {content}
-                </div>
-            : null }
         </div>
     );
 }
