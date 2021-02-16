61f1a19cbbf56d847e83e22ddd1b5455b841f947
Alexandre CHETRIT <chetrit.pro@hotmail.com>
2021-02-17 00:28:05 +0100

    feat: refactoring review components

---
+++ src/components/review/ChangeRequests.js
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import ClearIcon from '@material-ui/icons/Clear';
+import Button from '@material-ui/core/Button';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { ChangeRequest } from './ChangeRequest';
+
+const useStyles = makeStyles((theme) => ({
+    deleteChangeRequest: {
+        float: 'right'
+    },
+    wrappers: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        overflowWrap: 'break-word',
+    },
+    right: {
+        width: '20%',
+        float: 'right',
+    },
+    left: {
+        width: '20%',
+        float: 'left'
+    }
+}));
+
+export function ChangeRequests() {
+    const [changeRequests, setChangeRequests] = React.useState(null);
+
+    const classes = useStyles();
+
+    const handleClickDeleteChangeRequest = (e, index) => {
+        let newArr = [...changeRequests];
+
+        newArr.splice(index, 1);
+        setChangeRequests(newArr.length === 0 ? null : newArr);
+    }
+    const handleClickAddChangeRequest = (e) => {
+        let newArr;
+        if (changeRequests) {
+            newArr = [...changeRequests];
+            newArr.push({content: undefined, line: undefined, page: undefined});
+        } else {
+            newArr = [{content: undefined, line: undefined, page: undefined}];
+        }
+        setChangeRequests(newArr)
+    }
+    const handleClickDone = (e) => {
+        alert('Done Clicked');
+    }
+
+    return (
+        <div id="change_requests">
+            { changeRequests ? changeRequests.map((value , index) => {
+                return (
+                    <div key={index} id="change_request">
+                        <Button className={classes.deleteChangeRequest}
+                            onClick={(e) => {handleClickDeleteChangeRequest(e, index)}}>
+                            <ClearIcon/>
+                        </Button>
+                        <br/>
+                        <ChangeRequest
+                            changeRequests={changeRequests}
+                            setChangeRequests={setChangeRequests}
+                            value={value}
+                            index={index}
+                        />
+                        <p><br/></p>
+                    </div>
+                )
+            }) : <p>No change requests</p> }
+            <div style={{marginRight: '5%', marginLeft: '5%'}}>
+                <div className={classes.wrapper}>
+                    <span className={classes.left}><p></p></span>
+                    <Button
+                        variant='outlined'
+                        onClick={handleClickAddChangeRequest}
+                    >
+                        Add Change Request
+                    </Button>
+                    <Button
+                        className={classes.right}
+                        onClick={handleClickDone}
+                        variant='contained'
+                        color='primary'
+                    >
+                        Done
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
